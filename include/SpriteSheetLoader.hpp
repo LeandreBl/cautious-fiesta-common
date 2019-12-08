3945e3c8ea39fbecfaaed9c7645afa7f84228105
jb <jean-baptiste.melet@epitech.eu>
2019-12-08 07:46:26 +0100

    [fix] oublie d'une methode dans SpriteSheet

--- include/SpriteSheetLoader.hpp
+++ include/SpriteSheetLoader.hpp
@@ -9,6 +9,7 @@ namespace cf
         public:
            SpriteSheetLoader(const std::string &path) noexcept;
            std::vector<sf::IntRect> getFrames() const noexcept {return _frames;};
+           std::string getSpritePath() const noexcept {return _spritePath;};
         protected:
             std::vector<sf::IntRect> _frames;
             std::string _spritePath;
