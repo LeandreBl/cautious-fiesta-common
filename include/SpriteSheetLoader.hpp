f0d865d078d6dc64fa16474f10c115d4915fd5a2
Ludovic <ludovic.porokhov@epitech.eu>
2019-12-09 19:08:41 +0100

    normed

--- include/SpriteSheetLoader.hpp
+++ include/SpriteSheetLoader.hpp
@@ -4,14 +4,15 @@
 
 namespace cf
 {
-    class SpriteSheetLoader
-    {
-        public:
-           SpriteSheetLoader(const std::string &path) noexcept;
-           std::vector<sf::IntRect> getFrames() const noexcept {return _frames;};
-           std::string getSpritePath() const noexcept {return _spritePath;};
-        protected:
-            std::vector<sf::IntRect> _frames;
-            std::string _spritePath;
-    };
-}
+class SpriteSheetLoader
+{
+public:
+    SpriteSheetLoader(const std::string &path) noexcept;
+    std::vector<sf::IntRect> getFrames() const noexcept { return _frames; };
+    std::string getSpritePath() const noexcept { return _spritePath; };
+
+protected:
+    std::vector<sf::IntRect> _frames;
+    std::string _spritePath;
+};
+} // namespace cf
